5c1238826277d3a9d32e706d6481443af07ad515
kunatenshi <kunatenshi@localhost.localdomain>
2024-07-27 15:29:50 -0400

    stop on adding parameter

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,1 +1,13 @@
+#!/home/kunatenshi/miniconda3/bin/python3
+#Purpose: Say Hello
+
+import argparse
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('name', help='Name to greet')
+args = parser.parse_args()
+print('Hello, ' + args.name + '!')
+
+
+
+
 print('Hello, World!')
